d0f9aba96bf68004d0f1b069051324f521d6d8a5
Ivan Vynogradov <vaniavinogradov@rambler.ru>
2016-09-22 09:07:52 +0300

    first commit

--- src/main/java/ru/javaops/masterjava/Main.java
+++ src/main/java/ru/javaops/masterjava/Main.java
@@ -9,6 +9,6 @@ package ru.javaops.masterjava;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello MasterJava!");
+        System.out.format("Hello MasterJava1!");
     }
 }
